d0d3eb55caa575f88196d7ec7397567656993e13
Sofia Cheung <sofiavictoria17@gmail.com>
2020-03-08 21:20:24 -0700

    kth smallest node in binary search tree using Morris traversal

---
+++ BinaryTrees/kthSmallestInBST.py
@@ -0,0 +1,47 @@
+"""
+class Tree(object):
+  def __init__(self, x):
+     self.value = x
+     self.left = None
+     self.right = None
+"""
+
+
+def kthSmallestInBST(t, k):
+    """
+        Finds the kth element of a binary search tree
+        Implementation of In-order traversal using Morris Traversal
+        Args:
+            @root: root node of a binary tree
+            @k: an integer
+        Returns:
+            The value of the kth node of the binary search tree
+    """
+    counter = 0
+    current = t
+    while(current is not None):
+        # a node that can be threaded has been reached, print
+        if current.left is None:
+            print(current.value)
+            counter += 1
+            if counter == k:
+                return current.value
+            current = current.right
+        else:
+            pre = current.left
+            # Get the rightmost child
+            while(pre.right is not None and pre.right != current):
+                pre = pre.right
+
+            # rightmost child will be threaded to current
+            if(pre.right is None):
+                pre.right = current
+                current = current.left
+            # Otherwise, the node is threaded, print and fix it back
+            else:
+                pre.right = None
+                print(current.value)
+                counter += 1
+                if counter == k:
+                    return current.value
+                current = current.right
