e1df06a5d939c438c9082d85e414a126ca41f928
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-27 17:45:46 -0700

    Finishing TreeNode class

--- LeetCode/Trees/TreeSetup.py
+++ LeetCode/Trees/TreeSetup.py
@@ -1,11 +1,15 @@
 """
-Binary Tree structure
+Binary Tree structure SetUp
 """
 
 
 class TreeNode:
     """
     A binary tree class called TreeNode that follows leetcode format
+    Args:
+        val: value of the node
+        left: left node
+        right: right node
     """
     def __init__(self, x):
         self.val = x
@@ -13,9 +17,43 @@ class TreeNode:
         self.right = None
 
 
+def printTree(root, space=0):
+    """Prints binary tree horizontally
+    """
+    if (root is None):
+        return
+
+    space += 10
+    printTree(root.right, space)
+    print()
+    for i in range(10, space):
+        print(end=" ")
+    print(root.val)
+    printTree(root.left, space)
+
+
 def toBinaryTree(array):
+    """
+    Creates a binary tree based on LeetCode's format (array to tree)
+    Args:
+        array: Array representing binary tree in level order
+    Return: root node of the created binary tree
+    """
     if array:
         stack = [None if i is None else TreeNode(i) for i in array]
+    children = stack[::-1]
+    root = children.pop()
+    for node in stack:
+        if node:
+            if children:
+                node.left = children.pop()
+                node.right = children.pop()
+    return root
 
 
-tree = toBinaryTree([3, 9, 20, None, None, 15, 7])
+"""
+Pass input array to toBinaryTree to create tree
+Solve more leet code problems using this tree
+"""
+# tree = toBinaryTree([3, 9, 20, None, None, 15, 7])
+# printTree(tree)
